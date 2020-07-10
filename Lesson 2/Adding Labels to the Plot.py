cef9f623e629a7f85b158946296b405700f113fc
aipython <aipython.india@gmail.com>
2020-07-10 20:09:31 +0530

    Lesson 2

---
+++ Lesson 2/Adding Labels to the Plot.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Importing Modules (Library)
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+
+
+# ### Assigning X & Y axis label
+
+# In[3]:
+
+
+x_data = [1,2,3,4,5]
+y_data = [1, 4, 9, 16, 25]
+plt.plot(x_data, y_data)
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.show()
+
+
+# In[ ]:
+
+
+
+
